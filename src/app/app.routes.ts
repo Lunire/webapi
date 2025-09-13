79b2d1a54b2ced6f75e59d42f4f915aceb83f48b
Lunire <66011212136@msu.ac.th>
2025-09-14 05:01:26 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { AsyncDemo } from './pages/async-demo/async-demo';
 import { CallApi } from './pages/call-api/call-api';
 import { Postput } from './pages/postput/postput';
-import { Delete } from './pages/delete/delete';
 import { Update } from './pages/update/update';
 import { Upload } from './pages/upload/upload';
 
@@ -10,7 +9,6 @@ export const routes: Routes = [
     {path: '', component: AsyncDemo },
     {path: 'callapi', component: CallApi},
     {path: 'put', component: Postput},
-    {path: 'delete', component: Delete},
     {path: 'update/:id', component: Update},
     {path: 'upload', component: Upload},
 ];
